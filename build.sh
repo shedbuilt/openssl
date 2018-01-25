83826febb748f3d8f12d3ba841a9f16b947a2bd8
Auston Stewart <auston@shedbuilt.net>
2018-01-24 15:21:30 -1000

    Updating to 1.1.0g

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
          --openssldir=/etc/ssl \
          --libdir=lib          \
          shared                \
-         zlib-dynamic
-make -j $SHED_NUMJOBS
+         zlib-dynamic && \
+make -j $SHED_NUMJOBS || exit 1
 # Do not install static libraries
-sed -i 's# libcrypto.a##;s# libssl.a##;/INSTALL_LIBS/s#libcrypto.a##' Makefile
-make DESTDIR=${SHED_FAKEROOT} MANSUFFIX=ssl install
-mv -v ${SHED_FAKEROOT}/usr/share/doc/openssl{,-1.1.0f}
-cp -vfr doc/* ${SHED_FAKEROOT}/usr/share/doc/openssl-1.1.0f
+sed -i 's# libcrypto.a##;s# libssl.a##;/INSTALL_LIBS/s#libcrypto.a##' Makefile && \
+make DESTDIR="$SHED_FAKEROOT" MANSUFFIX=ssl install || exit 1
+mv -v "${SHED_FAKEROOT}"/usr/share/doc/openssl{,-1.1.0g}
+cp -vfr doc/* "${SHED_FAKEROOT}/usr/share/doc/openssl-1.1.0g"
