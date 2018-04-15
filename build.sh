8c5688798bdfb1eac26646490bfa50ac345d8832
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:26:48 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
          --libdir=lib          \
          shared                \
          zlib-dynamic && \
-make -j $SHED_NUMJOBS || exit 1
+make -j $SHED_NUM_JOBS || exit 1
 # Do not install static libraries
 sed -i 's# libcrypto.a##;s# libssl.a##;/INSTALL_LIBS/s#libcrypto.a##' Makefile && \
-make DESTDIR="$SHED_FAKEROOT" MANSUFFIX=ssl install || exit 1
-mv -v "${SHED_FAKEROOT}"/usr/share/doc/openssl{,-1.1.0g}
-cp -vfr doc/* "${SHED_FAKEROOT}/usr/share/doc/openssl-1.1.0g"
+make DESTDIR="$SHED_FAKE_ROOT" MANSUFFIX=ssl install || exit 1
+mv -v "${SHED_FAKE_ROOT}"/usr/share/doc/openssl{,-1.1.0g}
+cp -vfr doc/* "${SHED_FAKE_ROOT}/usr/share/doc/openssl-1.1.0g"
