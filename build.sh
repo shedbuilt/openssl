5ae4c21ea5fc25e5885cd458b9b4a263d326c79a
Auston Stewart <auston@shedbuilt.net>
2018-07-04 11:16:06 -1000

    Fixing doc installation

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
-SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 # Configure
 ./config --prefix=/usr         \
          --openssldir=/etc/ssl \
@@ -14,8 +13,8 @@ sed -i '/INSTALL_LIBS/s/libcrypto.a libssl.a//' Makefile &&
 make DESTDIR="$SHED_FAKE_ROOT" MANSUFFIX=ssl install || exit 1
 # Install Documentation
 if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
-    mv -v "${SHED_FAKE_ROOT}/usr/share/doc/openssl" "$SHED_PKG_LOCAL_DOCDIR" &&
-    cp -vfr doc/* "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}" || exit 1
+    mv -v "${SHED_FAKE_ROOT}/usr/share/doc/openssl" "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}" &&
+    cp -vfr doc/* "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}" || exit 1
 else
     rm -rf "${SHED_FAKE_ROOT}/usr/share/doc/openssl"
 fi
